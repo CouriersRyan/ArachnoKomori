3f3ae4607b34ce1dda80f00333ae6e6f5b1e1f47
Ryan Zhang <zhangryan09@gmail.com>
2021-05-23 16:24:58 -0700

    Reformatted Code
    Used Rider's context actions

--- ArachnoKomori/Assets/Scripts/Unit.cs
+++ ArachnoKomori/Assets/Scripts/Unit.cs
@@ -1,63 +1,59 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Unit : MonoBehaviour
 {
+    [SerializeField] private float speed = 5;
+
     /*
      why as custom objects rather than Vector2?
      in the future I plan on having Nodes that can move. Nodes account
      for all behaviors, such as attacking, so it is necessary for them
      to be more than Vector2 objects.
      */
-    private List<GameObject> nodePath;
-    
-    private Rigidbody2D rbody;
+    private List<GameObject> _nodePath;
 
-    [SerializeField] float speed = 5;
-    
+    private Rigidbody2D _rbody;
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        rbody = GetComponent<Rigidbody2D>();
-        nodePath = new List<GameObject>();
+        _rbody = GetComponent<Rigidbody2D>();
+        _nodePath = new List<GameObject>();
     }
-    
-    void FixedUpdate()
+
+    private void FixedUpdate()
     {
-        if(nodePath.Count > 0) MoveOnPath();
+        if (_nodePath.Count > 0) MoveOnPath();
     }
 
     public void OverwritePath(GameObject node)
     {
-        nodePath = new List<GameObject>();
-        nodePath.Add(node);
+        _nodePath = new List<GameObject> {node};
     }
 
     public void AppendPath(GameObject node)
     {
-        nodePath.Add(node);
+        _nodePath.Add(node);
     }
 
     public List<GameObject> GetPath()
     {
-        return nodePath;
+        return _nodePath;
     }
 
-    void MoveOnPath()
+    private void MoveOnPath()
     {
-        var nodePos = nodePath[0].transform.position;
-        Vector2 direction = (nodePos - transform.position);
+        var nodePos = _nodePath[0].transform.position;
+        Vector2 direction = nodePos - transform.position;
         if (direction.magnitude > 1) direction = direction.normalized;
 
-        rbody.velocity = direction * speed;
+        _rbody.velocity = direction * speed;
 
-        if (direction.magnitude < 0.05)
-        {
-            //TODO: object pooling to save space.
-            Destroy(nodePath[0]);
-            nodePath.Remove(nodePath[0]);
-            rbody.velocity = Vector2.zero;
-        }
+        if (!(direction.magnitude < 0.05)) return;
+        //TODO: object pooling to save space.
+        Destroy(_nodePath[0]);
+        _nodePath.Remove(_nodePath[0]);
+        _rbody.velocity = Vector2.zero;
     }
 }
