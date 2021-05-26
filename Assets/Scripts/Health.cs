73e48979ed5a83ca349551979abd9f01d3b0e228
kyle zhang <zkyle1337@gmail.com>
2021-05-25 23:54:48 -0700

    added health class

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public float startingHealth;
+    private float _currHealth;
+
+    public float currHealth => _currHealth;
+
+    private void Awake()
+    {
+        _currHealth = startingHealth;
+    }
+
+    public void ChangeHealth(float delta)
+    {
+        _currHealth += delta;
+    }
+}
